21f5b1474399bc5d9277ab78e0e6a7027d5f1d1a
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-15 22:41:15 +0300

    second

--- hw-7-t1/src/index.js
+++ hw-7-t1/src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Products extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    // ...
+    this.setState({
+      products: [
+        { id: 1, namen: 'Товар 1', price: 1000 },
+        { id: 2, namen: 'Товар 2', price: 2000 },
+        { id: 3, namen: 'Товар 3', price: 3000 },
+        { id: 4, namen: 'Товар 4', price: 4000 },
+        { id: 5, namen: 'Товар 5', price: 5000 }
+      ]
+    })
+  }
+
+  create = (id, namen, price) => this.setState({
+    products: [
+      ...this.state.products,
+      { id: id, namen: namen, price: price }
+    ]
+  })
+
+  update = (targetId, namen, price) => this.setState({
+    products: this.state.products.map(product => (
+      product.id !== targetId ? product : {
+        id: targetId, namen: namen, price: price
+      }
+    ))
+  })
+
+  delete = (targetId) => this.setState({
+    products: this.state.products.filter(({ id }) => id !== targetId)
+  })
+
+  render() {
+    const { products } = this.state
+    return(
+      <table className="products">
+        <thead>
+          <tr>
+            <th className="products__th">#</th>
+            <th className="products__th">Название</th>
+            <th className="products__th">Цена</th>
+            <th className="products__th">Действия</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(({ id, namen, price }) => 
+            <tr key={id}>
+              <td className="products__td">{id}</td>
+              <td className="products__td">{namen}</td>
+              <td className="products__td">{price}</td>
+              <td className="products__td">
+                <button
+                  className="products__btn"
+                  onClick={() => this.delete(id)}
+                >Удалить</button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Products/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
